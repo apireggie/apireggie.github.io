c4cd7c110e5098b0f8acb8d417af0f55f2845fcc
Reginald Appiah <reginald.appiah7@gmail.com>
2025-07-16 17:11:11 -0400

    🚀 Auto-commit: minor update

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,7 @@ class APIReggieCanvasApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true),
       home: RepositoryProvider(
-          //?
-          create: (_) => CanvasRepository(),
-          child: Container()),
+          create: (_) => CanvasRepository(), child: Container()),
     );
   }
 }

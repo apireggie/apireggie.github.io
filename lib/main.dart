337ebda19ea2e513e9d9c4d6caaa706e32c67a5b
Reginald Appiah <reginald.appiah7@gmail.com>
2025-07-16 14:15:42 -0400

    🚀 Auto-commit: minor update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'canvas_repository.dart';
 
 const openAiKey = String.fromEnvironment('OPENAI_API_KEY');

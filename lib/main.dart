dc6da51b545b05f2a3727639574daae0ce7f3adb
Reginald Appiah <reginald.appiah7@gmail.com>
2025-07-16 14:55:19 -0400

    🚀 Auto-commit: minor update

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'canvas_repository.dart';
 
-const openAiKey = String.fromEnvironment('OPENAI_API_KEY');
-const canvasApiKey = String.fromEnvironment('CANVAS_API_KEY');
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 

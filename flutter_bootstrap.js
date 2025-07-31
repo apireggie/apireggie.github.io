e2c8d1c76a18f67189978804420284ea630365a3
reginaldappiah <reginaldappiah@users.noreply.github.com>
2025-07-31 00:44:05 +0000

    deploy: apireggie/suite@d3018cb25715cddaa658c0f308d00f0915bc9ba9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3502320103"
+    serviceWorkerVersion: "3720289122"
   }
 });
